7727c8f73a4f5d5fadda26741fe3c3b949512e84
akpabioignatius <1414987@rgu.ac.uk>
2016-03-08 15:13:06 +0000

    db

--- connection.php
+++ connection.php
@@ -1,15 +1,18 @@
+<?php require("include/constants.php"); ?>
 <?php
-/**
- * Created by PhpStorm.
- * User: akpabioignatius
- * Date: 08/03/2016
- * Time: 2:47 PM
- */
-$connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
+$connection = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
 if (!$connection){
-    die("Database Connection Failed:" .mysql_errno());
+
+    die("Database Connection Failed:" .mysql_error());
+
 }
+
+
 $db_select = mysql_select_db(DB_NAME, $connection);
 if (!$db_select){
+
     die("Database Connection Failed:" .mysql_error());
+
 }
+
+?>
